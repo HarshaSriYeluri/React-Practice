d3d602314d2225637503bac158e46e275934d69b
SriHarsha.Yeluri <SriHarsha.Yeluri@ivycomptech.com>
2025-02-04 09:16:55 +0530

    set state and api call in class component

--- src/About.js
+++ src/About.js
@@ -1,15 +1,22 @@
 import React from "react";
+import ShimmerUI from "./ShimmerUI";
 
 class About extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 0
+            count: 0,
+            profile: {}
         }
     }
 
-    componentDidMount() {
+    async componentDidMount() {
         // calls when component render
+        const response = await fetch('https://api.github.com/users/HarshaSriYeluri');
+        const json = await response.json();
+        this.setState({
+            profile: json
+        })
     }
 
     componentDidUpdate() {
@@ -21,9 +28,9 @@ class About extends React.Component {
     }
 
     render() {
-        const { count } = this.state;
-        const {name, location, contact} = this.props;
-        return (
+        const { count, profile: {name, location, company} } = this.state;
+        const {contact} = this.props;
+        return (Object.keys(this.state.profile).length === 0) ? <ShimmerUI /> : (
             <div className="about">
                 <h1>Views: {count} <button onClick={() => {
                     this.setState({
@@ -32,7 +39,7 @@ class About extends React.Component {
                 }} style={{cursor: "pointer"}}>+</button></h1>
                 <div>
                     <h1>Name: {name}</h1>
-                    <h2>Location: {location}</h2>
+                    <h2>Company: {company}, {location}</h2>
                     <h3>Contact: {contact}</h3>
                 </div>
             </div>
